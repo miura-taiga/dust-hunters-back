469632a4939a4aa96d50d18d2628f004bc29d8d7
三浦大河 <taigav8@gmail.com>
2024-09-04 18:27:13 +0900

    デプロイテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
   validates :name, presence: true
   validates :gender, presence: true
   validates :hunterrank, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 999 }
-  
+
   enum gender: { male: 'male', female: 'female', other: 'other' }
 end

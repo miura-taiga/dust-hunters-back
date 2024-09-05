79114593bac60cf59cb309059f9379d970eaeb65
三浦大河 <taigav8@gmail.com>
2024-09-05 19:50:44 +0900

    usersコントローラ及びserializerの作成

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,40 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
+  def index
+    users = User.all
+    render json: users, each_serializer: Api::V1::UserSerializer
+  end
+
+  def show
+    render json: @user, serializer: Api::V1::UserSerializer
+  end
+
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
   def current
     if @current_user
       render json: { user: @current_user }
     else
       render json: { error: '認証情報を取得できません' }, status: :unauthorized
     end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: 'ユーザーが見つかりません' }, status: :not_found
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :gender)
   end
 end

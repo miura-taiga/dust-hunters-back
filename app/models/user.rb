368bcd01e96a1648c14b00da7df486f19387f2da
三浦大河 <taigav8@gmail.com>
2024-09-06 16:22:45 +0900

    usersテーブルからuid削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  validates :uid, presence: true, uniqueness: true
+  has_many :user_authentications
+
   validates :email, presence: true, uniqueness: true
   validates :name, presence: true
   validates :gender, presence: true

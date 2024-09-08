848ba92b7ad75132cdf746bddb332d91da32474d
三浦大河 <taigav8@gmail.com>
2024-09-08 19:49:25 +0900

    serializerの明示を変更

--- app/controllers/api/v1/quests_controller.rb
+++ app/controllers/api/v1/quests_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::QuestsController < ApplicationController
   def index
     @monsters = Monster.all
-    render json: @monsters, each_serializer: MonsterSerializer
+    render json: @monsters, each_serializer: QuestSerializer
   end
 
   def show
     @monster = Monster.find_by!(id: params[:id])
-    render json: @monster, serializer: MonsterSerializer
+    render json: @monster, serializer: QuestSerializer
   rescue ActiveRecord::RecordNotFound
     render json: { error: 'モンスターが見つかりませんでした' }, status: :not_found
   end

0f20820e66610fbcbb2c3568242f4bd48c3f2498
三浦大河 <taigav8@gmail.com>
2024-09-11 17:58:45 +0900

    MonsterSerializerにbodyを追加

--- app/serializers/monster_serializer.rb
+++ app/serializers/monster_serializer.rb
@@ -1,3 +1,3 @@
 class MonsterSerializer < ActiveModel::Serializer
-  attributes :id, :name, :start_battle_image_url, :end_battle_image_url, :bestiary_monster_image_url
+  attributes :id, :name, :body, :start_battle_image_url, :end_battle_image_url, :bestiary_monster_image_url
 end

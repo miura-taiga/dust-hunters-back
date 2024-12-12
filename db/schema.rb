84f828aaca9189ecf29aa5a87520c86a8e2acbce
三浦大河 <taigav8@gmail.com>
2024-12-12 13:54:58 +0900

    usersテーブルからemailカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_12_043848) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_12_044510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,13 +76,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_12_043848) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", null: false
     t.string "name", limit: 10, null: false
     t.integer "hunterRank", default: 1
     t.string "gender", default: "male", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email"
   end
 
   add_foreign_key "defeated_records", "quests"

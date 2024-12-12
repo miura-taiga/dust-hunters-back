e762f32e4abb5f3ab02baf170ec4564def388aec
三浦大河 <taigav8@gmail.com>
2024-12-12 13:37:34 +0900

    questsテーブルのbodyカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_09_060315) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_12_042857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_09_060315) do
 
   create_table "quests", force: :cascade do |t|
     t.string "title", null: false
-    t.text "body"
     t.bigint "monster_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
